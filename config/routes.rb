223f643fb508f52102f5a0815af4f71d0eae76e2
Toshifumi Kurosawa <kuromailserver@gmail.com>
2023-09-15 21:47:23 +0000

    add

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'posts/new'
   devise_for :users
-  root 'home#top'
+  root 'posts#index'
   resources :posts, only: [ :index, :new, :create, :show, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
