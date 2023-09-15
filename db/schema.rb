c7ca798b36d52473513fb949de3af30db9e53596
Toshifumi Kurosawa <kuromailserver@gmail.com>
2023-09-15 21:06:23 +0000

    add

--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_15_203328) do
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "content", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -32,4 +36,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_15_203328) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
