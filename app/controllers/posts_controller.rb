1442cb57fed6cc2b266a2e6e8152536a491cdd7c
Toshifumi Kurosawa <kuromailserver@gmail.com>
2023-09-15 21:13:54 +0000

    add

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:show, :index] 
 
   def new
     @post = Post.new
@@ -15,6 +15,14 @@ class PostsController < ApplicationController
       flash[:alert] = '投稿に失敗しました'
       render :new
     end
+  end
+
+  def show # 追加
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def index # 追加
+    @posts = Post.limit(10).order(created_at: :desc)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'posts/new'
   devise_for :users
   root 'home#top'
-  resources :posts, only: [:new, :create]
+  resources :posts, only: [:new, :create, :show, :index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
